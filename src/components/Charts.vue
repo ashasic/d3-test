d087fe0c5f40377f75bddc52f5f54c463bcb91f3
nhoque <naimul-hoque@uiowa.edu>
2025-09-18 16:28:52 -0500

    added bar chart

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -9,7 +9,7 @@ import * as d3 from "d3";
 const chart = ref(null);
 
 // Example data
-const dataset = [30, 80, 45, 60, 20, 90, 50];
+const dataset = [5, 10, 20, 25, 30];
 
 onMounted(() => {
   drawChart();
@@ -25,14 +25,77 @@ function drawChart() {
     .attr("width", width)
     .attr("height", height);
 
-  svg.selectAll("text")
+  // Example data
+const dataset = [5, 10, 25, 30, 35];
+
+// svg.selectAll("text") //select all text in the html (none so far)
+//    .data(dataset) //count and parses all data
+//    .enter() //creates new, data-bound elements (placeholders) for the data values
+//    .append("text") // takes the empty elements and enters text elements
+//    .attr("x", 20) // x position
+//    .attr("y", (d, i) => 20 + i * 20) // stack vertically
+//    .text(d => "I can count up to "+ d) //text content
+//    .attr("fill", d => {if (d<60) return "red"; else return "black";}); //style
+
+  // var circles = svg.selectAll("circle")
+  //   .data(dataset)
+  //   .enter()
+  //   .append("circle"); // now circles are appended to the end of the SVG element
+
+  // circles.attr("cx", function(d, i) {return (i * 50) + 25;})
+  //   .attr("cy", height/2)
+  //   .attr("r", function(d) {return d;})
+  //   .attr("fill", "yellow")
+  //   .attr("stroke", "orange")
+  //   .attr("stroke-width", function(d) {return d/2;});
+
+  // svg.selectAll("text")
+  //  .data(dataset)
+  //  .enter()
+  //  .append("text")
+  //  .attr("x", 20)
+  //  .attr("dy", (d, i) => 20 + i * 20)
+  //  .text(d => "I can count up to "+ d)
+  //  .attr("fill", d => {if (d > 10) return "red"; else return "black";});
+
+  const x = d3.scaleBand()
+    .domain(dataset.map((_, i) => i))
+    .range([margin.left, width - margin.right])
+    .padding(0.1);
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(dataset)])
+    .nice()
+    .range([height - margin.bottom, margin.top]);
+
+  var bars = svg.append("g")
+    .attr("fill", "steelblue")
+    .selectAll("rect")
     .data(dataset)
-    .enter()
-    .append("text")
-    .attr("x", 20) // x position
-    .attr("dy", (d, i) => 20 + i * 20) // stack vertically
-    .text("A new text")
-    .attr("fill", "black");
+    .join("rect")
+      .attr("x", (_, i) => x(i))
+      .attr("y", d => y(d))
+      .attr("height", d => y(0) - y(d))
+      .attr("width", x.bandwidth());
+  
+  bars.on("mouseover", function(event, d) {
+    d3.select(this).attr("fill", "orange")
+  })
+
+  bars.on("mouseout", function(event, d) {
+    d3.select(this).attr("fill", "steelblue")
+  })
+
+  // X Axis
+  svg.append("g")
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+    .call(d3.axisBottom(x).tickFormat(i => i + 1).tickSizeOuter(0));
+
+  // Y Axis
+  svg.append("g")
+    .attr("transform", `translate(${margin.left},0)`)
+    .call(d3.axisLeft(y));
+
 }
 </script>
 
