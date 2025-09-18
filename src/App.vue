ca17f416716fd8f74d993e407c5fc50818177cee
nhoque <naimul-hoque@uiowa.edu>
2025-09-18 10:24:43 -0500

    started new repo

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+
+    <div class="row">
+      <div class="col">
+        <h1>Sample Vue App with Bootstrap</h1>
+      </div>
+    </div>
+
+    <hr />
+
+    <div class="row">
+      <div class="col">
+        <p>This is a paragraph.</p>
+      </div>
+    </div>
+
+    <hr />
+
+    <div class="row">
+      <div class="col">
+        <p>This could be a footer</p>
+      </div>
+    </div>
+  </div>
+</template>
+
